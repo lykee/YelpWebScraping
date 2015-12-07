cb98789f3d16d3c56c75774a9ef07396c3d22efd
morvarid <mariah.hamdi@gmail.com>
2015-12-07 01:33:33 -0800

    M2- updating searchterm input file

--- restaurant.py
+++ restaurant.py
@@ -8,41 +8,50 @@
 		...
 		...
 # usage: restaurant.py [-h] -f FILENAME -s SEARCHTERM
+
 	Scrape Data through Yelp API
+
 	optional arguments:
 	  -h, --help            show this help message and exit
 	  -f FILENAME, --fileName FILENAME
 	                        Name of file containing neighbourhoods and their
 	                        respective cities in a pipe-delimited fashion
 	  -s SEARCHTERM, --searchTerm SEARCHTERM
-	                        Search Category for the data. eg. restaurants, bars,
+	                        Name of text file containing search Categories for the data. eg. restaurants, bars,
 	                        chinese, etc.
-# Output file: formed in 'output' folder
+
+
 '''
 
 
 import argparse
-from pprint import pprint
 import json
 import sys
 import urllib
 import urllib2
 import oauth2
 import config
-import csv
 import time
 import getlocation
+from pymongo.errors import ConnectionFailure
+from pymongo import MongoClient
+
 
 # Global Variables Declaration
 API_HOST	= "api.yelp.com"
 SEARCH_PATH	= "/v2/search"
 BUSINESS_PATH	= "/v2/business/"
+# Number of business results to return
 SEARCH_LIMIT 	= 20
+# Offset the list of returned business results by this amount
 OFFSET_LIMIT	= 0
-SORT_TYPE	= 2
+# Sort mode: 0=Best matched (default), 1=Distance, 2=Highest Rated.
+SORT_TYPE	= 0
+#fetch 40 records for each neighborhood in city mentioned
 MAX_LIMIT	= 40
-TERM		= 'restaurants'
+
 INPUT_FILE_NAME	= ''
+TERM_FILE_NAME=''
 
 # Yelp API Keys from config file
 CONSUMER_KEY	= config.consumerKey
@@ -72,32 +81,41 @@ def request(host, path, urlParams=None):
 	
 	try:
 		response = json.loads(conn.read())
+
 	finally:
 		conn.close()
 	
 	return response	
 
 
+
 # Declare search parameters to be passed to API
-def search(location, longitude, latitude):
+def search(term,location, longitude, latitude):
 
 	urlParams = {
 			'location'	: location,
 			'cll'		: str(latitude) + ', ' + str(longitude),
-			'term'		: TERM,
+			'term'		: term,
 			'limit'		: SEARCH_LIMIT,
 			'offset'	: OFFSET_LIMIT,
 			'sort'		: SORT_TYPE
 		}
 	return request(API_HOST, SEARCH_PATH, urlParams)
-	
 
-# Write Data to output file.
-def writeData(location, dataCategory):
-	with open('output/' + TERM + '.csv', 'a') as f:
-		fieldNames = ['ID', 'NAME', 'REVIEW_COUNT', 'RATING', 'LONGITUDE', 'LATITUDE', 'CITY', 'STATE', 'ZIP', 'COUNTRY', 'CATEGORY']	
-		writer = csv.DictWriter(f, fieldnames=fieldNames, delimiter='|')
-		writer.writerows(dataCategory)
+
+
+def mongoConnect():
+	global dbh
+
+	""" Connect to MongoDB """
+	try:
+		c = MongoClient(host="localhost", port=27017)
+		print "\n Connected successfully to MongoDB \n"
+	except ConnectionFailure, e:
+		sys.stderr.write("Could not connect to MongoDB: %s" % e)
+		sys.exit(1)
+	dbh = c["lykee"]
+
 	
 
 # Read the input file and transform the data -  creates a dictionary with CityName as key and NeighbourhoodNames list as its value
@@ -116,91 +134,76 @@ def getallNeighbourhoodData():
 	return neighbourhood
 
 
-# Scrape Data for each Neighbourhood
-def queryApi(city, neighbourhood = ''):
-	restaurantData = []
-	categoryData = []
-	tempRestaurantData = {}
-	tempCategoryData = {}
+# Scrape Data for each Neighbourhood and term
+def queryApi(term,city, neighbourhood = ''):
+
 	global OFFSET_LIMIT
 	global MAX_LIMIT
+	total_inserted=0
 	location = neighbourhood + ', ' + city + ', US'
-
 	# Get longitude and latitude from Google Geocoding API V3 
-        longitude, latitude = getlocation.getCoordinates(location)
+	longitude, latitude = getlocation.getCoordinates(location)
+	
 	print location, longitude, latitude
 
 	# Call API twice for each neighbourhood (API response restricted to 20 records for each request) 
 	while OFFSET_LIMIT < MAX_LIMIT:
-		response = search(location, longitude, latitude)
+		response = search(term,location, longitude, latitude)
 		MAX_LIMIT = response['total']
 
 		allRestaurantData = response['businesses']
-		if len(allRestaurantData) > 0:
+		n_biz=len(allRestaurantData) 
+		if n_biz > 0:
+			
 			for restaurant in allRestaurantData:
-				tempRestaurantData['ID']		= restaurant['id'].encode('ascii', 'ignore')
-				tempRestaurantData['NAME']		= restaurant['name'].encode('ascii', 'ignore')
-				tempRestaurantData['REVIEW_COUNT']	= restaurant['review_count']
-				tempRestaurantData['RATING']		= restaurant['rating']
-				if restaurant['location'].get('coordinate'):
-					tempRestaurantData['LONGITUDE']		= restaurant['location']['coordinate']['longitude']
-					tempRestaurantData['LATITUDE']		= restaurant['location']['coordinate']['latitude']
-				else:
-	                                tempRestaurantData['LONGITUDE']         = 0.0
-        	                        tempRestaurantData['LATITUDE']          = 0.0
-				tempRestaurantData['CITY']		= restaurant['location']['city']
-				tempRestaurantData['STATE']		= restaurant['location']['state_code']
-				tempRestaurantData['ZIP']		= restaurant['location']['postal_code'] if restaurant['location'].get('postal_code') else 99999
-				tempRestaurantData['COUNTRY']		= restaurant['location']['country_code']
 				
-				tempRestaurantData['CATEGORY']		= ''	
-
-				# If Categories are present, store them as comma seperated strings
-				if restaurant.get('categories'):
-					for category in restaurant['categories']:
-						categoryData.append(category[1].encode('ascii', 'ignore'))					
-					
-					tempRestaurantData['CATEGORY'] = ",".join(categoryData)
-					categoryData = []
-
-			 	restaurantData.append(tempRestaurantData)
-				tempRestaurantData = {}
-	
+				dbh.restaurant.insert(restaurant)#, safe=True)
+				
 			time.sleep(4)
 			OFFSET_LIMIT += 20
+			total_inserted+=n_biz
 	
 	# Write data for each neighbourhood. Maximum of 40 records
-	print 'Writing {0} records'.format(OFFSET_LIMIT)
-	writeData(city, restaurantData)
+	print 'Writing {0} records for term {1}'.format(total_inserted,term.strip())
+	print 'Total number documents in the collection {0} \n\n'.format(dbh.restaurant.find().count())
+	
 	OFFSET_LIMIT = 0
 
 # Main function
 def main():
 
-
 	parser = argparse.ArgumentParser(description='Scrape Data through Yelp API');
 	parser.add_argument('-f', '--fileName', dest='fileName', type=str, help='Name of file containing neighbourhoods and their respective cities in a pipe-delimited fashion', required=True)
-	parser.add_argument('-s', '--searchTerm', dest='searchTerm', type=str, help='Search Category for the data. eg. restaurants, bars, chinese, etc. ', required=True)
+	parser.add_argument('-s', '--searchTerm', dest='searchTerm', type=str, help='Name of text file containing search Categories for the data. eg. restaurants, bars, chinese, etc. ', required=True)
 	inputValues = parser.parse_args()
 
 	global INPUT_FILE_NAME 
-	global TERM
-	INPUT_FILE_NAME = inputValues.fileName
-	TERM = inputValues.searchTerm
+	global TERM_FILE_NAME
+	global term
 
+	
+	mongoConnect()
+	INPUT_FILE_NAME = inputValues.fileName
+	TERM_FILE_NAME = inputValues.searchTerm
+	with open(TERM_FILE_NAME, 'r') as t:
+		terms=t.readlines()
 	allCities = getallNeighbourhoodData()
 	
 	# For each neighbourhood in each city, get highest rated restaurants 
-	for city, neighbourhoods in allCities.items():
-		for neighbourhood in neighbourhoods:			
-		        try:
-				queryApi(city, neighbourhood)
-			except urllib2.HTTPError as error:
-		                sys.exit('Encountered HTTP error {0}. Abort Program.'.format(error.code))
+	
+	
+	for term in terms:
+		for city, neighbourhoods in allCities.items():
+			for neighbourhood in neighbourhoods:
+				try:
+					queryApi(term,city, neighbourhood)
+				except urllib2.HTTPError as error:
+					sys.exit('Encountered HTTP error {0}. Abort Program.'.format(error.code))
+
 
 		# Once all neighbourhoods data for each city is collected, fetch 40 highest rated restaurants in the city. Some restaurants that don't specify neighbourhoods would be skipped in 
 		# above API request call
-		queryApi(city)
+		#queryApi(term,city)
 
 		
 if __name__ == '__main__':
